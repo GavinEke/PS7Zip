a65e4436a6e66743184bec34e055ecdc94b49e47
Gavin Eke <GavinEke@users.noreply.github.com>
2016-05-24 12:59:30 +1000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,8 +1,9 @@
 $projecturl = "https://github.com/GavinEke/PS7Zip/archive/master.zip"
 $tmpzip = "$env:TEMP\PS7Zip.zip"
 $profilemodules = "$env:USERPROFILE\Documents\WindowsPowerShell\modules"
-Remove-Item $tmpzip
+Remove-Item $tmpzip -ErrorAction SilentlyContinue
 (New-Object System.Net.WebClient).DownloadFile($projecturl, $tmpzip)
 Unblock-File -Path $tmpzip
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory($tmpzip, $profilemodules)
+Rename-Item $profilemodules\PS7Zip-master -NewName PS7Zip
