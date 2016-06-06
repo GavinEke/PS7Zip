bdbc755919639216b01e1cbf894d05956a0c944a
Gavin Eke <GavinEke@users.noreply.github.com>
2016-06-06 13:20:17 +1000

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,10 +1,13 @@
-$projecturl = "https://github.com/GavinEke/PS7Zip/archive/master.zip"
-$tmpfolder = "$env:TEMP"
-$tmpzip = "$tmpfolder\PS7Zip.zip"
-$profilemodules = "$env:USERPROFILE\Documents\WindowsPowerShell\modules"
-(New-Object System.Net.WebClient).DownloadFile($projecturl, $tmpzip)
-Unblock-File -Path $tmpzip
+#Requires -Version 3
+$GitHubUser = "GavinEke"
+$RepoName = "PS7Zip"
+$ProfileModulePath = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules"
+If (!(Test-Path $ProfileModulePath)) {
+    Net-Item -ItemType Directory -Path $ProfileModulePath
+}
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/$GitHubUser/$RepoName/archive/master.zip", "$env:TEMP\$RepoName.zip")
+Unblock-File -Path "$env:TEMP\$RepoName.zip"
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory($tmpzip, $env:TEMP)
-Move-Item -Path "$tmpfolder\PS7Zip-master\PS7Zip" -Destination "$profilemodules"
-Remove-Item "$tmpzip\PS7Zip*" -Recurse -ErrorAction SilentlyContinue
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$env:TEMP\$RepoName.zip", "$env:TEMP")
+Move-Item -Path "$env:TEMP\$RepoName-master\$RepoName" -Destination "$ProfileModulePath"
+Remove-Item "$env:TEMP\$RepoName*" -Recurse -ErrorAction SilentlyContinue
