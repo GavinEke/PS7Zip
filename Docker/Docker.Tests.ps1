5aad58e86dc3aec57a967deb805cd77718526a7d
Gavin Eke <GavinEke@users.noreply.github.com>
2017-05-31 15:04:28 +1000

    Update Docker.Tests.ps1

--- Docker/Docker.Tests.ps1
+++ Docker/Docker.Tests.ps1
@@ -1,6 +1,6 @@
 $GitHubUser = "GavinEke"
 $RepoName = "PS7Zip"
 $ProfileModulePath = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules"
-Invoke-WebRequest -Uri "https://github.com/$GitHubUser/$RepoName/archive/master.zip" -UseBasicParsing -OutFile "c:\$RepoName.zip"
-Expand-Archive -Path "c:\$RepoName.zip" -DestinationPath "$ProfileModulePath"
+Invoke-WebRequest -Uri "https://github.com/$GitHubUser/$RepoName/archive/master.zip" -UseBasicParsing -OutFile "C:\$RepoName.zip"
+Expand-Archive -Path "C:\$RepoName.zip" -DestinationPath "$ProfileModulePath"
 Invoke-Pester @Verbose -Path "$ProfileModulePath\PS7Zip-master\PS7Zip\Tests" 
