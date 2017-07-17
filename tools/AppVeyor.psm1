f3d20a2f0b3ca9139ceae4afd0114c56ee01cb94
Gavin Eke <GavinEke@users.noreply.github.com>
2017-07-17 21:43:48 +1000

    Update AppVeyor.psm1

--- tools/AppVeyor.psm1
+++ tools/AppVeyor.psm1
@@ -1,13 +1,8 @@
-Write-Host -ForegroundColor Magenta -Object "--- Start of AppVeyor.psm1 ---"
-
 $ErrorActionPreference = 'Stop'
 $ProjectRoot = $env:APPVEYOR_BUILD_FOLDER
 $ProjectName = $env:APPVEYOR_PROJECT_NAME
 
 Write-Host -ForegroundColor Yellow -Object "PowerShell Version is $($PSVersionTable.PSVersion)"
-Write-Host -ForegroundColor Yellow -Object "Project Root is $ProjectRoot"
-Write-Host -ForegroundColor Yellow -Object "Project Name is $ProjectName"
-Write-Host -ForegroundColor Yellow -Object "AppVeyor Job ID is $env:APPVEYOR_JOB_ID"
 
 Function Invoke-AppVeyorInstall {
     If ($PSVersionTable.PSVersion -ge [Version]'5.0') {
@@ -19,6 +14,10 @@ Function Invoke-AppVeyorInstall {
         nuget install platyPS -source https://www.powershellgallery.com/api/v2 -outputDirectory "$Env:ProgramFiles\WindowsPowerShell\Modules\."
         nuget install PSScriptAnalyzer -source https://www.powershellgallery.com/api/v2 -outputDirectory "$Env:ProgramFiles\WindowsPowerShell\Modules\."
     }
+    
+    Write-Host -ForegroundColor Yellow -Object "Pester version is $(Get-Module -Name Pester -ListAvailable | Select-Object -ExpandProperty Version)"
+    Write-Host -ForegroundColor Yellow -Object "platyPS version is $(Get-Module -Name platyPS -ListAvailable | Select-Object -ExpandProperty Version)"
+    Write-Host -ForegroundColor Yellow -Object "PSScriptAnalyzer version is $(Get-Module -Name PSScriptAnalyzer -ListAvailable | Select-Object -ExpandProperty Version)"
 }
 
 Function Invoke-AppVeyorTest {
@@ -53,7 +52,7 @@ Function Invoke-AppVeyorTest {
         Start-Process -FilePath msiexec.exe -ArgumentList '-qn','-i C:\PowerShell-win10-x64.msi','-norestart' -wait
         $PSCore_EXE = Get-Item -Path $Env:ProgramFiles\PowerShell\*\powershell.exe
         New-Item -Type SymbolicLink -Path $Env:ProgramFiles\PowerShell\ -Name latest -Value $PSCore_EXE.DirectoryName
-        & "C:\Program Files\PowerShell\latest\PowerShell.exe" -Command Invoke-Pester @Verbose -Path "$ProjectRoot\$ProjectName\Tests"
+        & "C:\Program Files\PowerShell\latest\PowerShell.exe" -Command Invoke-Pester @Verbose -Path "$ProjectRoot\$ProjectName\Tests -OutputFormat NUnitXml -OutputFile "$ProjectRoot\TestResults_PSCoreTest.xml" -PassThru | Export-Clixml -Path "$ProjectRoot\PesterResults_PSCoreTest.xml""
     }
 
     If ($AltInstallTest) {
@@ -65,4 +64,36 @@ Function Invoke-AppVeyorTest {
     }
 }
 
-Write-Host -ForegroundColor Magenta -Object "--- End of AppVeyor.psm1 ---"
+Function Invoke-AppVeyorBuild {
+    Import-Module -Path "$ProjectRoot\$ProjectName" -Force -ErrorAction SilentlyContinue
+    New-MarkdownHelp -Module $ProjectName -OutputFolder "$ProjectRoot\$ProjectName\docs"
+}
+
+Function Invoke-AppVeyorDeploy {
+    [CmdletBinding()]
+    param(
+        [switch]$DeployToGallery,
+        [switch]$DeployToArtifacts
+    )
+    Update-ModuleManifest -Path "$ProjectRoot\$ProjectName\$ProjectName.psd1" -ModuleVersion "$env:APPVEYOR_BUILD_VERSION"
+    Import-Module -Path "$ProjectRoot\$ProjectName" -Force -ErrorAction SilentlyContinue
+    
+    If ($DeployToGallery) {
+        Write-Host -ForegroundColor Yellow -Object "Deploying to PSGallery"
+        If ($PSVersionTable.PSVersion.Major -ge 5) {
+            [Version]$GalleryVersion = Find-Package $ProjectName -ErrorAction Stop | Select-Object -ExpandProperty Version
+            [Version]$LocalVersion = Get-Module $ProjectName -ErrorAction Stop | Select-Object -ExpandProperty Version
+
+            If (($LocalVersion.Major -gt $GalleryVersion.Major) -or ($LocalVersion.Minor -gt $GalleryVersion.Minor)) {
+                Write-Host -ForegroundColor Yellow -Object "Deploying $LocalVersion to the PowerShell Gallery"
+                Publish-Module -Path "$ProjectRoot\PS7Zip" -NuGetApiKey "$env:NuGetApiKey"
+            }
+        }
+    }
+    
+    If ($DeployToArtifacts) {
+        Write-Host -ForegroundColor Yellow -Object "Deploying to AppVeyor Artifacts"
+        Compress-Archive -Path "$ProjectRoot\$ProjectName" -DestinationPath "$ProjectRoot\$ProjectName-$LocalVersion.zip"
+        Push-AppveyorArtifact "$ProjectRoot\$ProjectName-$LocalVersion.zip"
+    }
+}
