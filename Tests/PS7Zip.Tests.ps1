4cba17ae51ad4f85af718bafbb6527b905bb8f4d
Gavin Eke <gavineke-github@yahoo.com.au>
2017-11-05 16:30:35 +1100

    Update Tests

--- Tests/PS7Zip.Tests.ps1
+++ Tests/PS7Zip.Tests.ps1
@@ -15,10 +15,10 @@ Describe "PS7Zip Module - $OSVersion" {
         It 'Should not have any PSScriptAnalyzer warnings' {
             Import-Module PSScriptAnalyzer -Force -ErrorAction SilentlyContinue
             $ScriptWarnings = Invoke-ScriptAnalyzer -Path "$PSScriptRoot\..\$ModuleName" -Recurse
-            $ScriptWarnings.Length | Should BeNullOrEmpty
+            $ScriptWarnings | Should BeNullOrEmpty
         }
 		
-		$script:manifest = $null
+        $script:manifest = $null
         It "has a valid manifest" {
             { $script:manifest = Test-ModuleManifest -Path "$PSScriptRoot\..\$ModuleName\$ModuleName.psd1" -ErrorAction Stop -WarningAction SilentlyContinue } | Should Not Throw
         }
@@ -28,28 +28,27 @@ Describe "PS7Zip Module - $OSVersion" {
 Describe "Compress-7Zip Function - $OSVersion" {
     Context 'Strict mode' {
         Set-StrictMode -Version latest
-        Set-Location TestDrive:
 
-        It 'should create archive.zip in the TestDrive' {
-            New-Item TestDrive:\archive -ItemType Directory
-            Compress-7Zip TestDrive:\archive
-            Test-Path TestDrive:\archive.zip | Should Be $True
+        It 'should create archive.zip in the current working folder' {
+            New-Item archive -ItemType Directory
+            Compress-7Zip archive
+            Test-Path archive.zip | Should Be $True
             Remove-Item archive*
-		}
+        }
 		
-		It 'should create a gzip archive of a single file and delete the uncompressed file' {
-            New-Item TestDrive:\archive.csv -ItemType File
-            New-Item TestDrive:\folder -ItemType Directory
-            Compress-7Zip 'TestDrive:\archive.csv' -OutputFile 'TestDrive:\folder\files.gz' -ArchiveType GZIP -Remove
-            Test-Path TestDrive:\folder\files.gz | Should Be $True
-            Test-Path TestDrive:\archive.csv | Should Be $False
-		}
+        It 'should create a gzip archive of a single file and delete the uncompressed file' {
+            New-Item archive.csv -ItemType File
+            New-Item folder -ItemType Directory
+            Compress-7Zip 'archive.csv' -OutputFile 'folder\files.gz' -ArchiveType GZIP -Remove
+            Test-Path folder\files.gz | Should Be $True
+            Test-Path archive.csv | Should Be $False
+        }
 		
-		It 'should create an archive based on pipeline input' {
-            New-Item TestDrive:\archive.txt -ItemType File
-            Get-ChildItem TestDrive:\archive.txt | Compress-7Zip
-            Test-Path TestDrive:\archive.zip | Should Be $True
-            Remove-Item TestDrive:\archive.txt
+        It 'should create an archive based on pipeline input' {
+            New-Item archive.txt -ItemType File
+            Get-ChildItem archive.txt | Compress-7Zip
+            Test-Path archive.zip | Should Be $True
+            Remove-Item archive.txt
         }
     }
 }
@@ -57,18 +56,17 @@ Describe "Compress-7Zip Function - $OSVersion" {
 Describe "Expand-7Zip Function - $OSVersion" {
     Context 'Strict mode' {
         Set-StrictMode -Version latest
-        Set-Location TestDrive:
 
-        It 'should extract contents of archive.zip in the TestDrive' {
-            Expand-7Zip TestDrive:\archive.zip
-            Test-Path TestDrive:\archive.txt | Should Be $True
-            Remove-Item TestDrive:\archive.txt
-		}
+        It 'should extract contents of archive.zip in the current working folder' {
+            Expand-7Zip archive.zip
+            Test-Path archive.txt | Should Be $True
+            Remove-Item archive.txt
+        }
 		
-		It 'should extract contents of TestDrive:\folder\files.gz into current working folder' {
-            Expand-7Zip 'TestDrive:\folder\files.gz'
-            Test-Path TestDrive:\archive.csv | Should Be $True
-            Remove-Item TestDrive:\archive.csv
+        It 'should extract contents of folder\files.gz into the current working folder' {
+            Expand-7Zip 'folder\files.gz'
+            Test-Path archive.csv | Should Be $True
+            Remove-Item archive.csv
         }
     }
 }
@@ -76,20 +74,19 @@ Describe "Expand-7Zip Function - $OSVersion" {
 Describe "Get-7Zip Function - $OSVersion" {
     Context 'Strict mode' {
         Set-StrictMode -Version latest
-        Set-Location TestDrive:
-        
+
         It 'should list contents of archive.zip in the TestDrive' {
-            $get7ziptest1 = Get-7Zip TestDrive:\archive.zip
+            $get7ziptest1 = Get-7Zip archive.zip
             Test-Path Variable:get7ziptest1 | Should Be $True
             Write-Output "$get7ziptest1"
-            Remove-Item TestDrive:\archive*
-		}
+            Remove-Item archive*
+        }
 		
-		It 'should list contents of TestDrive:\folder\files.gz' {
-            $get7ziptest2 = Get-7Zip 'TestDrive:\folder\files.gz'
+        It 'should list contents of folder\files.gz' {
+            $get7ziptest2 = Get-7Zip 'folder\files.gz'
             Test-Path Variable:get7ziptest2 | Should Be $True
             Write-Output "$get7ziptest2"
-            Remove-Item TestDrive:\folder -Recurse
+            Remove-Item folder -Recurse
         }
     }
 }
