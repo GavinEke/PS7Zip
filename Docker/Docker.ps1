d9caa2c6b4bc8e0d9c739e28d7178c6c0a344125
Gavin Eke <gavineke-github@yahoo.com.au>
2017-11-05 16:35:46 +1100

    Update Docker Items

--- Docker/Docker.ps1
+++ Docker/Docker.ps1
@@ -3,4 +3,7 @@ $RepoName = "PS7Zip"
 $ProfileModulePath = "$env:USERPROFILE\Documents\WindowsPowerShell\Modules"
 Invoke-WebRequest -Uri "https://github.com/$GitHubUser/$RepoName/archive/master.zip" -UseBasicParsing -OutFile "C:\$RepoName.zip"
 Expand-Archive -Path "C:\$RepoName.zip" -DestinationPath "$ProfileModulePath"
+Install-PackageProvider -Name NuGet -Force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Install-Module -Name Pester, PSScriptAnalyzer -SkipPublisherCheck -Force
 Invoke-Pester -Path "$ProfileModulePath\$RepoName-master\Tests" -EnableExit
